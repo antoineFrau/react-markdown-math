cca7a1650388e6e7d2c165983add9f00f5b59d44
Antoine Frau <antoine@korbit.ai>
2020-10-19 14:52:38 +0200

    Markdown-it and mathjax

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
 import React from 'react'
+import "./App.css"
 import MarkdownRender from "./MarkdownRender"
+// enable everything
+var md = require('markdown-it')();
 
 function App(props) {
   const source= `Lift($L$) can be determined by Lift Coefficient ($C_L$) like the following
-  equation.
-  
-  $$
-  L = \frac{1}{2} \rho v^2 S C_L
-  $$`
+equation.
+$\\sin(2\\pi)$
+$$
+L = \\frac{1}{2} \\rho v^2 $S C_L
+$$\n
+
+# Live demo
+
+$\\sum_{i=0}^{10} x^{2}$
+
+# Live demo
+
+Changes are automatically rendered as you type.
+
+* Implements [GitHub Flavored Markdown](https://github.github.com/gfm/)
+* Renders actual, "native" React DOM elements
+* Allows you to escape or skip HTML (try toggling the checkboxes above)
+* If you escape or skip the HTML, no \`dangerouslySetInnerHTML\` is used! Yay!
+x
+## How about some code?
+
+\`\`\`py
+import math
+
+def pow(v):
+    return v**2
+\`\`\`
+
+Pretty neat, eh?
+
+## Tables?
+
+|Feature|Support|
+|-------|-------|
+|tables| ✔ |
+|alignment| ✔ |
+|wewt| ✔ |
+
+## More info?
+
+Read usage information and more on [GitHub](https://github.com/remarkjs/react-markdown)
+
+---------------
+
+A component by [Espen Hovlandsdal](https://espen.codes/)
+`
+  var result = md.render(source);
+
   return (
-    <div>
+    // <div className="markdown-body">
+
+    // </div>
+    // {result}
+    <div className="markdown-body">
       <MarkdownRender source={source}></MarkdownRender>
     </div>
   )
--- src/MarkdownRender.js
+++ src/MarkdownRender.js
@@ -2,27 +2,36 @@ import React from 'react'
 import ReactMarkdown from 'react-markdown';
 import MathJax from 'react-mathjax'
 import RemarkMathPlugin from 'remark-math';
+import gfm from 'remark-gfm'
+import { Light as SyntaxHighlighter } from 'react-syntax-highlighter';
+import python from 'react-syntax-highlighter/dist/esm/languages/hljs/python';
+import solarizedDark from 'react-syntax-highlighter/dist/esm/styles/hljs/solarized-dark';
 
+SyntaxHighlighter.registerLanguage('python', python);
 function MarkdownRender(props) {
     const newProps = {
         ...props,
         plugins: [
           RemarkMathPlugin,
+          [gfm, {singleTilde: false}]
         ],
         renderers: {
             ...props.renderers,
             math: ({ value }) => {
-                console.log("math equation")
-                return <MathJax.Node formula={props.value} />
+                return <MathJax.Node formula={value} />
             },
             inlineMath: ({ value }) =>{
-                console.log("azeaz")
-                return <MathJax.Node inline formula={props.value} />
+                return <MathJax.Node inline formula={value} />
+            },
+            code: ({language, value}) => {
+              return <SyntaxHighlighter style={solarizedDark} language={language} children={value} showLineNumbers={true} />
             }
         }
       };
       return (
-        <ReactMarkdown {...newProps}/>
+        <MathJax.Provider>
+          <ReactMarkdown {...newProps} rawSourcePos={true}/>
+        </MathJax.Provider>
       );
 }
 
